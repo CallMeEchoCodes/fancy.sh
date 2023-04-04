a831bcd22ea456554d2d96127bcb5f82f322b8fc
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2023-04-04 12:44:56 +1000

    :tada: Begin the project!

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fancy.sh
+temp readme
---
+++ fancy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+spinner_pid=
+
+function start_spinner {
+    trap "stop_spinner \\\e[91m\\\e[0m '$1'" EXIT
+    { 
+        while : ; do for X in '\e[93m|\e[0m' '\e[93m/\e[0m' '\e[93m-\e[0m' '\e[93m\\\e[0m';
+            do 
+                echo -ne "\r\033[0K"
+                echo -en "$X $1";
+                sleep 0.1;
+            done;
+        done &
+    } 2>/dev/null
+    spinner_pid=$!
+}
+
+function stop_spinner {
+    { kill -9 $spinner_pid && wait; } 2>/dev/null
+    echo -en "\033[2K\r"
+    echo -e "$1 $2"
+    trap - EXIT
+}
+
+function spinner {
+    start_spinner "$2"
+    cmdout="$1"
+    if ! $cmdout; then 
+        exit 1
+    fi
+    stop_spinner "\e[92m\e[0m $2"
+}
